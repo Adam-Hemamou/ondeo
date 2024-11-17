350130eaef60034fa290d0eab0a351554d0b4373
Adam HEMAMOU <h-mamou@hotmail.fr>
2024-11-17 17:03:48 +0100

    new scroll animation for logo gg wp

---
+++ src/app/logo-band/logo-band.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-band',
+  standalone: true,
+  imports: [],
+  templateUrl: './logo-band.component.html',
+  styleUrls: ['./logo-band.component.scss'],
+})
+export class LogoBandComponent {}
