f4c6b98dc50e8c8a730433c50b57468bb5a6e555
Adam HEMAMOU <h-mamou@hotmail.fr>
2025-02-27 17:00:22 +0100

    update tag manager

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import AOS from 'aos';
 
@@ -7,49 +6,13 @@ import AOS from 'aos';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  template: `<router-outlet></router-outlet>
-    <noscript>
-      <iframe
-        [attr.src]="safeGtmUrl"
-        height="0"
-        width="0"
-        style="display: none; visibility: hidden"
-      ></iframe>
-    </noscript>`,
+  template: `<router-outlet></router-outlet>`,
 })
 export class AppComponent implements OnInit {
-  gtmId = import.meta.env['VITE_GTM_ID'];
-  safeGtmUrl!: SafeResourceUrl;
-
-  private sanitizer = inject(DomSanitizer);
-
   ngOnInit() {
-    if (this.gtmId) {
-      const rawUrl = `https://www.googletagmanager.com/ns.html?id=${this.gtmId}`;
-      this.safeGtmUrl = this.sanitizer.bypassSecurityTrustResourceUrl(rawUrl);
-    }
-
     AOS.init({
       duration: 1200,
       once: true,
     });
-
-    if (this.gtmId) {
-      const script = document.createElement('script');
-      script.async = true;
-      script.innerHTML = `
-        (function(w,d,s,l,i){
-          w[l]=w[l]||[];
-          w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
-          var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),
-              dl=l!='dataLayer'?'&l='+l:'';
-          j.async=true;
-          j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-          f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${this.gtmId}');
-      `;
-      document.head.appendChild(script);
-    }
   }
 }
